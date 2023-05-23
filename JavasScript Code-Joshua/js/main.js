8c9e532a991d34e4cbe40d66026c9138faec68ea
Joshua <128613586+muitoquente@users.noreply.github.com>
2023-05-23 09:05:43 -0600

    operaciones java

--- JavasScript Code-Joshua/js/main.js
+++ JavasScript Code-Joshua/js/main.js
@@ -1,3 +1,18 @@
 
         console.log("Hello world");
-        alert("Bienvenido a JavaScript");
+       // alert("Bienvenido a JavaScript");
+        /*let nombre=prompt("Cual es tu nombre?");
+        console.log(nombre)
+        console.log(parseInt(nombre))
+        console.log (typeof(nombre))*/
+        let numerouno=prompt("ingresa un numero");
+        let numerodos=prompt("ingresa otro numero");
+
+        let resultadoSuma=parseInt(numerouno)+ parseInt(numerodos);
+        let resultadoResta=parseInt(numerouno)- parseInt(numerodos);
+        let resultadoMulti= parseInt(numerouno)*parseInt(numerodos);
+        let resultadodiv= parseInt(numerouno)/parseInt(numerodos);
+        console.log(`el resultado de la suma es: ${resultadoSuma}`);
+        console.log(`el resultado de la resta es: ${resultadoResta}`);
+        console.log(`el resultado de la multiplicacion es: ${resultadoMulti}`);
+        console.log(`el resultado de la division es: ${resultadodiv}`);
